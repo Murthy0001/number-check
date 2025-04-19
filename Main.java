c2f2b95b2b250338fc79415476674bee0d36bfd9
Murthy0001 <murthydadi999@gmail.com>
2025-04-19 14:36:31 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		if(a==0){
+		    System.out.println("zero");
+		}
+		else if(a>0){
+		    System.out.println("positive data");
+		}else{
+		    System.out.println("Negative data");
+		}
+	}
+}
